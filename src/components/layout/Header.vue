ab705a8c005ac5ab95e9482d918ebb0db02955df
iblurdesigner <nevomen@gmail.com>
2018-02-05 12:45:39 -0500

    hasta aqui con el sistema de componentes

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong Platzimusic en el header
+          .nav-right.nav-menu
+    .hero-body
+      .container.has-text-centered
+        h1.title Platzi Music
+        h2.subtitle Canciones que estan buenisimas
+        pm-player
+</template>
+
+<script>
+  import PmPlayer from '@/components/Player.vue'
+  export default {
+    components: { PmPlayer }
+  }
+</script>
