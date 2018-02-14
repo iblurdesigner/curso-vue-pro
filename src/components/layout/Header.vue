86bcc31165b50840b3f4a95921a0de0db1816324
iblurdesigner <nevomen@gmail.com>
2018-02-14 09:11:22 -0500

    Internacionalizacion de textos con i18n

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -7,8 +7,10 @@
             .nav-item
               strong 📻  Platzi Music
             .nav-right.nav-menu
-              router-link.nav-item(:to="{ name: 'search' }") Buscar
-              router-link.nav-item(to="about") Nosotros
+              a.nav-item(@click="selectLang('en')") EN
+              a.nav-item(@click="selectLang('es')") ES
+              router-link.nav-item(to="search") {{ $t('search') }}
+              router-link.nav-item(to="about") {{ $t('about') }}
 
     .hero-body
       .container.has-text-centered
@@ -21,6 +23,12 @@
 import PmPlayer from '@/components/Player.vue'
 
 export default {
-  components: { PmPlayer }
+  components: { PmPlayer },
+
+  methods: {
+    selectLang (lang) {
+      this.$i18n.locale = lang
+    }
+  }
 }
 </script>
